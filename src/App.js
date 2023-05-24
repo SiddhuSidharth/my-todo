90d07b3589a8c4c7e4dbb1aec588674c4e21130c
SiddhuSidharth <sidharthdharmalingam@gmail.com>
2023-05-24 10:48:19 +0530

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import  {useState} from "react";
 import './App.css';
+import {Task} from './Task';
 
 function App() {
   const [todoList,settodoList] = useState([]);
@@ -42,11 +43,11 @@ function App() {
         <div className="list">
          {todoList.map((task)=>{
           return(
-          <div>
-              <h1>{task.taskName}</h1>
-              <button onClick={ () => deleteTask(task.id)}>X</button>
-
-           </div>
+           <Task 
+           taskname={task.taskName} 
+           id={task.id} 
+           deleteTask={deleteTask}
+           />
           );  
 
          })}
